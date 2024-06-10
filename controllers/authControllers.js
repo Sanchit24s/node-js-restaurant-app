5a8abc83d98a2756676e7237533183f2e32923d1
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-06-11 01:46:39 +0530

    hash the password and create token

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/userModel");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const registerController = async (req, res) => {
     try {
@@ -19,10 +21,13 @@ const registerController = async (req, res) => {
             });
         }
 
+        var salt = bcrypt.genSaltSync(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const user = await userModel.create({
             userName,
             email,
-            password,
+            password: hashedPassword,
             phone,
             address
         });
@@ -52,17 +57,31 @@ const loginController = async (req, res) => {
             });
         }
 
-        const user = await userModel.findOne({ email: email, password: password });
+        const user = await userModel.findOne({ email });
         if (!user) {
             return res.status(404).send({
                 success: false,
-                message: 'User Not Found Or Password Mismatch'
+                message: 'User Not Found'
             });
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(500).send({
+                success: false,
+                message: 'Invalid Credentials'
+            });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '7d'
+        });
 
         res.status(200).send({
             success: true,
             message: 'Login Successful',
+            token,
             user
         });
     } catch (error) {
