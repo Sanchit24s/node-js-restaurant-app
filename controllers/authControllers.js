fb70e5795ee6eba326680fb2d805c820b8bf45d3
Sanchit24s <sanchit24bhoyar@gmail.com>
2024-06-11 01:14:14 +0530

    Add login route and controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -41,4 +41,38 @@ const registerController = async (req, res) => {
     }
 };
 
-module.exports = { registerController };
+const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(500).send({
+                success: false,
+                message: 'Please provide email or password'
+            });
+        }
+
+        const user = await userModel.findOne({ email: email, password: password });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: 'User Not Found Or Password Mismatch'
+            });
+        }
+
+        res.status(200).send({
+            success: true,
+            message: 'Login Successful',
+            user
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in login',
+            error
+        });
+    }
+};
+
+module.exports = { registerController, loginController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { registerController } = require('../controllers/authControllers');
+const { registerController, loginController } = require('../controllers/authControllers');
 
 const router = express.Router();
 
 router.post('/register', registerController);
+
+router.post('/login', loginController);
 
 module.exports = router;
